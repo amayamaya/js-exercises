b66b5d90117c7e236a4467199a9524a55bb9f2cd
‘AMAYA <aamaya547@gmail.com>
2022-04-19 16:44:01 -0700

    days in month + formatted name

--- app.js
+++ app.js
@@ -23,9 +23,15 @@ console.log(`areaOfTriangle(5, 10) === 25 : ${areaOfTriangle(5, 10) === 25}`);
 // 3. lessThan100
 // Given 2 numbers, return true if their sum is less than 100
 // return false if it is greater than 100
-function lessThan100(value) {
+function lessThan100(num1, num2) {
+    if (num1 + num2 < 100) {
+        return true;
+    } else {
+        return false;
+    }
+    }
+    
 
-}
 console.log(`lessThan100(5, 10) === true : ${lessThan100(5, 10) === true}`);
 console.log(`lessThan100(100, 10) === false : ${lessThan100(100, 10) === false}`);
 
@@ -34,7 +40,14 @@ console.log(`lessThan100(100, 10) === false : ${lessThan100(100, 10) === false}`
 // that accepts a first_name and last_name as required parameters
 // and a third parameter called middle_name which is optional.
 // The function should return a string with the first, middle and last names
-function getFormattedName() {}
+function getFormattedName(first_name, last_name, middle_name) {
+    if (!middle_name) {
+        return first_name + ' ' + last_name;
+    } else {
+        return first_name + ' ' + middle_name + ' ' + last_name; 
+    }
+}
+    
 console.log(
     `getFormattedName('Ruth', 'Ginsburg') === 'Ruth Ginsburg': ${
         getFormattedName('Ruth', 'Ginsburg') === 'Ruth Ginsburg'
@@ -56,7 +69,16 @@ console.log(
 //    that checks if the year is a leap year, and returns the correct
 //    number of days for Feb. The default value for the year should be
 //    the current year
-function daysInMonth() {}
+function daysInMonth(month) {
+    if (month === 2) {
+        return 29;
+    } if (month === 3) {
+        return 31;
+    } else {
+        return 30;
+    }
+} 
+
 console.log(`daysInMonth(2) === 29: ${daysInMonth(2) === 29}`);
 console.log(`daysInMonth(3) === 31: ${daysInMonth(3) === 31}`);
 console.log(`daysInMonth(11) === 30: ${daysInMonth(11) === 30}`);
